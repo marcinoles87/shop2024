fa9a2359839891c7a7923944aca3c77ab00699b8
marcinoles87 <marcinoles87@gmail.com>
2025-01-05 10:40:19 +0100

    add new component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 
 import {Route , Routes} from "react-router"
 import Nav from './components/Nav';
+import Header from './components/Header';
+import Main from './components/Main';
 
 
 function App() {
@@ -11,7 +13,10 @@ function App() {
       <Routes>
         <Route path='*' element={
           <>
+            <Header></Header>
             <Nav></Nav>
+            <Main></Main>
+
 
           </>
         }
