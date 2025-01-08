83a3bd81f27194f10382beff796147d1cb5f7563
marcinoles87 <marcinoles87@gmail.com>
2025-01-08 14:41:24 +0100

    add section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,46 @@
 import React from 'react'
 
+import img1 from '../img/logo.png'
+import img2 from '../img/logo.png'
+import img3 from '../img/logo.png'
+
 function Main() {
+
+  const mainElements = [
+    {
+      name : '1' ,
+      img : img1
+
+    } ,
+
+    {
+      name : '2' ,
+      img : img2
+
+    } ,
+
+    {
+      name : '3' ,
+      img : img3
+
+    } ,
+
+  ]
   return (
     <div className='main-cotnainer'>
       <div className='main-elements'>
 
-        <h1>ELEMENTY </h1>
+       
+
+        {mainElements.map( (item , index) => {
+          return(
+            <div className='main-element'>
+              <img src={item.img} alt={item.name}></img>
+              <p>{item.name}</p>
+              
+            </div>
+          )
+        })}
 
         {/* elementy pobrane do wyswietlenia z zew.API lub kolarz/zdjecia plus opis w ktory mozna wejsc --> nowa strona z opisem artykuly/produktu */}
       </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Product() {
   return (
     <div className='product-container'>
+      <h1>Products</h1>
         
     </div>
   )
