6de3613e831db6136c8f3acf5a5c9d133e3e126c
marcinoles87 <marcinoles87@gmail.com>
2025-01-10 15:39:55 +0100

    add axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import axios from 'axios'
+import { useEffect , useState} from 'react';
 
 import {Route , Routes} from "react-router"
 import Nav from './components/Nav';
@@ -9,6 +11,19 @@ import Product from './components/Product';
 
 
 function App() {
+
+  const [data , setData] = useState();
+
+  const baseUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/10'
+
+  useEffect( () => {
+    axios.get(`${baseUrl}`).then( (response) =>
+      setData(response.data)
+    
+    )
+ 
+      
+  } , [])
   return (
     <div className="App">
 
@@ -18,7 +33,7 @@ function App() {
             <Header></Header>
             <Nav></Nav>
             <Main></Main>
-            <Product></Product>
+            <Product data={data}></Product>
             <Contact></Contact>
 
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Product() {
+
+
+function Product(data) {
+
+
+  console.log(data)
+  
+
   return (
+
+
+
+  
     <div className='product-container'>
       <h1>Products</h1>
+
+      {data ? 
+
+      data.map( (item,index) => {
+        return(
+          <div className='product'>
+            <p>{item.artistDisplayName}</p>
+          </div>
+        )
+      })
+
+ : ""}
         
     </div>
   )
