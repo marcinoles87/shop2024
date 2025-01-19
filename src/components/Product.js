03a3eefb8e72386a6e65e18197dc35eda562f3b1
marcinoles87 <marcinoles87@gmail.com>
2025-01-19 09:12:44 +0100

    add new css

--- src/components/Product.js
+++ src/components/Product.js
@@ -16,11 +16,15 @@ function Product({data}) {
 
       {data.map ( (item,index) => {
                 return(
-                  <div className='product-one'>
-                  <p>{item.cell}</p>
-                  <p>{item.gender}</p>
-                  <p>{item.email}</p>
-                  <img src={item.picture.thumbnail} alt={item.email}></img>
+                  <div className='product-one' key={index}>
+                    <img src={item.picture.thumbnail} alt={item.email}></img>
+                    <div className='product-info'>
+                      <h4><b>{item.cell}</b></h4>
+                      <p>{item.gender}</p>
+                      <p>{item.email}</p>
+                    </div>
+                  
+                  
                   </div>
                 )
               })}
