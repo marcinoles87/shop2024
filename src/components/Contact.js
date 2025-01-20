9667c05b333719a99ce8c3f647f116705e80860c
marcinoles87 <marcinoles87@gmail.com>
2025-01-20 16:20:48 +0100

    add new css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ function Contact() {
         <h4>File</h4>
       </div>
 
-      <div className='contact-info2'>
+      <div className='contact-info'>
         <h4>Product</h4>
         <h4>Price</h4>
         <h4>API</h4>
