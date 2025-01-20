6c27136a19cade3e0b15c57da166193b3525a244
marcinoles87 <marcinoles87@gmail.com>
2025-01-20 16:09:29 +0100

    add new css

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,8 +4,42 @@ import React, { useEffect, useState } from 'react'
 
 function Product({data}) {
 
+  const r = Math.random()*300
+  const g = Math.random()*300
+  const b = Math.random()*300
+  const color = <div style={{width:'300px', height:'300px',backgroundColor:`rgb(${r},${g},${b})`}} ></div>
+
+
+
+console.log(color)
   
 
+  
+const products = [
+  {
+    name : 'name 1',
+    price : '99.99$',
+    img : color
+  },
+
+  {
+    name : 'name 1',
+    price : '99.99$',
+    img : color
+  },
+
+  {
+    name : 'name 1',
+    price : '99.99$',
+    img : color
+  },
+
+  {
+    name : 'name 1',
+    price : '99.99$',
+    img : color
+  }
+]
   
 
 
@@ -19,13 +53,21 @@ function Product({data}) {
                   <div className='product-one' key={index}>
                     <img src={item.picture.thumbnail} alt={item.email}></img>
                     <div className='product-info'>
-                      <h4><b>{item.cell}</b></h4>
-                      <p>{item.gender}</p>
+                      <h4><b>Product Name</b></h4>
+                      <p>Price : 99.99$</p>
                       <p>{item.email}</p>
                     </div>
                   
                   
                   </div>
+                )
+              })}
+
+              {products.map ( (item,index) => {
+                return(
+                  <>
+                  {item.img}
+                  </>
                 )
               })}
 
