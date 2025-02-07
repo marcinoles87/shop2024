cde578adaec17a0ee42632bf49ffbad125b1ba5c
marcinoles87 <marcinoles87@gmail.com>
2025-02-07 14:29:11 +0100

    add new function

--- src/pages/Koszyk.js
+++ src/pages/Koszyk.js
@@ -8,23 +8,29 @@ function Koszyk({koszyk , price}) {
   return (
     <div className='koszyk-container'>
       <div className='koszyk-items'>
-        <h2>Twoje zamowienie to : {zamowienieNr} z dnia : </h2>
+          <h2>Twoje zamowienie to : {zamowienieNr} z dnia : </h2>
           <p>{dzienZamowienia.toString()}</p>
-        <h3>Lista produktow :</h3>
-        {koszyk.map( (item,index) => {
-          return(
-            <div className='koszyk-produkty' key={index}>
-            <p>{item.index}</p>
-            <p>{item.email}</p>
-            <img src={item.picture.thumbnail} alt={item.index}></img>
-            </div>
-          )
-        })}
+          <h3>Lista produktow :</h3>
+              
+              {koszyk.map( (item,index) => {
+                return(
+                  <div className='koszyk-produkty' key={index}>
+                    <p>{item.index}</p>
+                    <p>{item.email}</p>
+                    <img src={item.picture.thumbnail} alt={item.index}></img>
+                  </div>
+                )
+              })}
 
-        <h3>Podsumowanie : </h3>
-        <h2>ilość produktów: {koszyk.length}</h2>
-        <h2>Wartośc produktów : </h2>
+   
       </div>
+
+      <div className='koszyk-podsumowanie'>
+          <h3>Podsumowanie : </h3>
+          <h2>ilość produktów: {koszyk.length}</h2>
+          <h2>Wartośc produktów : </h2>
+        </div>
+      
     </div>
   )
 }
