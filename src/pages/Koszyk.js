6d75d42f67e22e19674a509a2be3960ea042a05d
marcinoles87 <marcinoles87@gmail.com>
2025-02-12 16:07:02 +0100

    add new function

--- src/pages/Koszyk.js
+++ src/pages/Koszyk.js
@@ -5,9 +5,11 @@ function Koszyk({koszyk , price}) {
 
   const zamowienieNr = Math.floor(Math.random()*100)
   const dzienZamowienia = Date()
+
   return (
     <div className='koszyk-container'>
       <div className='koszyk-items'>
+          
           <h2>Twoje zamowienie to : {zamowienieNr} z dnia : </h2>
           <p>{dzienZamowienia.toString()}</p>
           <h3>Lista produktow :</h3>
@@ -17,6 +19,7 @@ function Koszyk({koszyk , price}) {
                   <div className='koszyk-produkty' key={index}>
                     <p>{item.index}</p>
                     <p>{item.email}</p>
+                    
                     <img src={item.picture.thumbnail} alt={item.index}></img>
                   </div>
                 )
@@ -26,6 +29,19 @@ function Koszyk({koszyk , price}) {
       </div>
 
       <div className='koszyk-podsumowanie'>
+          <h2>Twoje dane wysyłkowe :</h2>
+          <label>
+           Imie i Nazwisko <input onChange={(e) => e.target.value} ></input>
+          </label>
+
+          <label>
+           Adres wysyłki<input onChange={(e) => e.target.value}></input>
+          </label>
+
+          <label>
+           Telefon <input onChange={(e) => e.target.value}></input>
+          </label>
+          
           <h3>Podsumowanie : </h3>
           <h2>ilość produktów: {koszyk.length}</h2>
           <h2>Wartośc produktów : </h2>
