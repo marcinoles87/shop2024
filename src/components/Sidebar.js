fb9d6066ef41021f1025581c91bfcbd8cdb95111
marcinoles87 <marcinoles87@gmail.com>
2025-02-17 19:41:10 +0100

    add new function

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,7 +7,7 @@ function Sidebar() {
     const [color,setColor] = useState('')
 
     const handleFilter = () =>{
-        console.log(price,name,color)
+        console.log(name,price,color)
     }
 
   return (
@@ -15,17 +15,17 @@ function Sidebar() {
         <div className='sidebar-items'>
             <div className='sidebar-item'>
                 <label>Name</label>
-                <input onChange={setName( (e) => e.target.value)}></input>
+                <input onChange={e => setName(e.target.value)}></input>
             </div>
 
             <div className='sidebar-item'>
                 <label>Price</label>
-                <input onClick={setPrice( (e) => e.target.value)}></input>
+                <input onChange={e => setPrice(e.target.value)}></input>
             </div>
 
             <div className='sidebar-item'>
                 <label>Color</label>
-                <input onClick={setColor( (e) => e.target.value)}></input>
+                <input onChange={e => setColor(e.target.value)}></input>
             </div>
 
             <button onClick={handleFilter}>Filter</button>
