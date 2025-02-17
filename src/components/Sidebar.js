2ab8fcd3ef1382f6a1b72cc1462f1eb7292f9025
marcinoles87 <marcinoles87@gmail.com>
2025-02-17 19:35:21 +0100

    add new function

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,25 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Sidebar() {
+
+    const [price,setPrice] = useState('');
+    const [name,setName] = useState('');
+    const [color,setColor] = useState('')
+
   return (
     <div className='sidebar-container'>
         <div className='sidebar-items'>
             <div className='sidebar-item'>
                 <label>Name</label>
-                <input></input>
+                <input onChange={setName( (e) => e.target.value)}></input>
             </div>
 
             <div className='sidebar-item'>
                 <label>Price</label>
-                <input></input>
+                <input onClick={setPrice( (e) => e.target.value)}></input>
             </div>
 
             <div className='sidebar-item'>
                 <label>Color</label>
-                <input></input>
+                <input onClick={setColor( (e) => e.target.value)}></input>
             </div>
 
-            <button>Filter</button>
+            <button onClick={handleFilter}>Filter</button>
         </div>
     </div>
   )
